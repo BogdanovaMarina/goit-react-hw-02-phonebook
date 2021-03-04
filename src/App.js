e3228f1b89be1b343863a9950fc3829799126477
BogdanovaMarina <bogdanovamariwa@gmail.com>
2021-03-05 01:00:34 +0200

    init: start hw-phonebook

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './index.css';
+import Test from './components/Test';
+function App() {
+  return <Test />;
+}
+export default App;
---
+++ src/components/Test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import s from './Test.module.css';
+
+const Test = () => {
+  return <p className={s.title}>Hello homework-02- phonebook!!!!!</p>;
+};
+export default Test;
